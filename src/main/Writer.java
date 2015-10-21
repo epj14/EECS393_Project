ed2aa11e6290ad65b515a1fbabb00389acfc7df0
Evan Johnson <epj14@case.edu>
2015-10-21 17:47:21 -0400

    created project structure

---
+++ src/main/Writer.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * a class that runs the resume writer program
+ */
+public class Writer {
+	
+	/**
+	 * runs the program
+	 * @param args none
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
