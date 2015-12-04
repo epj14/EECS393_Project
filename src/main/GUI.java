daec6f9b69fde81dd8a563d921d6b69f74d4dacf
epj14 <epj14@case.edu>
2015-12-04 03:18:25 -0500

    integrated Searcher and GUI

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -11,6 +11,7 @@ import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -22,6 +23,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 import com.itextpdf.text.DocumentException;
@@ -33,6 +35,7 @@ public class GUI extends JFrame {
 	private ContentWriter cw;
 	private TemplateWriter tw;
 	private PDFWriter pw;
+	private Searcher s;
 	
 	private JPanel contentPane;
 	private JPanel panel_3;
@@ -44,6 +47,9 @@ public class GUI extends JFrame {
 	private JTextField textField_3;
 	private JTextField textField_4;
 	private JTextArea textArea;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JPanel panel_16;
 
 	/**
 	 * Launch the application.
@@ -74,14 +80,24 @@ public class GUI extends JFrame {
 		
 		JPanel MainMenu = new JPanel();
 		contentPane.add(MainMenu, "MainMenu");
-		GridBagLayout gbl_MainMenu = new GridBagLayout();
-		gbl_MainMenu.columnWidths = new int[]{0, 0};
-		gbl_MainMenu.rowHeights = new int[] {0, 0, 0, 0, 0, 0};
-		gbl_MainMenu.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gbl_MainMenu.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		MainMenu.setLayout(gbl_MainMenu);
+		MainMenu.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_17 = new JPanel();
+		MainMenu.add(panel_17);
+		GridBagLayout gbl_panel_17 = new GridBagLayout();
+		gbl_panel_17.columnWidths = new int[]{0};
+		gbl_panel_17.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_panel_17.columnWeights = new double[]{0.0};
+		gbl_panel_17.rowWeights = new double[]{0.0, 0.0};
+		panel_17.setLayout(gbl_panel_17);
 		
 		JButton btnCreateDocument = new JButton("Create Document");
+		GridBagConstraints gbc_btnCreateDocument = new GridBagConstraints();
+		gbc_btnCreateDocument.fill = GridBagConstraints.HORIZONTAL;;
+		gbc_btnCreateDocument.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCreateDocument.gridx = 0;
+		gbc_btnCreateDocument.gridy = 0;
+		panel_17.add(btnCreateDocument, gbc_btnCreateDocument);
 		btnCreateDocument.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -89,15 +105,14 @@ public class GUI extends JFrame {
 				cl.show(contentPane, "CreateDocument_1");
 			}
 		});
-		GridBagConstraints gbc_btnCreateDocument = new GridBagConstraints();
-		gbc_btnCreateDocument.anchor = GridBagConstraints.CENTER;
-		gbc_btnCreateDocument.gridwidth = GridBagConstraints.REMAINDER;
-		gbc_btnCreateDocument.insets = new Insets(0, 0, 5, 0);
-		gbc_btnCreateDocument.gridx = 0;
-		gbc_btnCreateDocument.gridy = 0;
-		MainMenu.add(btnCreateDocument, gbc_btnCreateDocument);
 		
 		JButton btnCreateTemplate = new JButton("Create Template");
+		GridBagConstraints gbc_btnCreateTemplate = new GridBagConstraints();
+		gbc_btnCreateTemplate.fill = GridBagConstraints.HORIZONTAL;;
+		gbc_btnCreateTemplate.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCreateTemplate.gridx = 0;
+		gbc_btnCreateTemplate.gridy = 1;
+		panel_17.add(btnCreateTemplate, gbc_btnCreateTemplate);
 		btnCreateTemplate.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -105,15 +120,29 @@ public class GUI extends JFrame {
 				cl.show(contentPane, "CreateTemplate");
 			}
 		});
-		GridBagConstraints gbc_btnCreateTemplate = new GridBagConstraints();
-		gbc_btnCreateTemplate.anchor = GridBagConstraints.CENTER;
-		gbc_btnCreateTemplate.gridwidth = GridBagConstraints.REMAINDER;
-		gbc_btnCreateTemplate.insets = new Insets(0, 0, 5, 0);
-		gbc_btnCreateTemplate.gridx = 0;
-		gbc_btnCreateTemplate.gridy = 1;
-		MainMenu.add(btnCreateTemplate, gbc_btnCreateTemplate);
+		
+		JButton btnSearch_1 = new JButton("Search");
+		btnSearch_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				CardLayout cl = (CardLayout)(contentPane.getLayout());
+				cl.show(contentPane, "SearchPanel");
+			}
+		});
+		GridBagConstraints gbc_btnSearch_1 = new GridBagConstraints();
+		gbc_btnSearch_1.fill = GridBagConstraints.HORIZONTAL;;
+		gbc_btnSearch_1.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSearch_1.gridx = 0;
+		gbc_btnSearch_1.gridy = 2;
+		panel_17.add(btnSearch_1, gbc_btnSearch_1);
 		
 		JButton btnHelp = new JButton("Help");
+		GridBagConstraints gbc_btnHelp = new GridBagConstraints();
+		gbc_btnHelp.fill = GridBagConstraints.HORIZONTAL;;
+		gbc_btnHelp.insets = new Insets(0, 0, 0, 5);
+		gbc_btnHelp.gridx = 0;
+		gbc_btnHelp.gridy = 3;
+		panel_17.add(btnHelp, gbc_btnHelp);
 		btnHelp.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -126,27 +155,19 @@ public class GUI extends JFrame {
 						"Choose Help to display this text again.");
 			}
 		});
-		GridBagConstraints gbc_btnHelp = new GridBagConstraints();
-		gbc_btnHelp.anchor = GridBagConstraints.CENTER;
-		gbc_btnHelp.gridwidth = GridBagConstraints.REMAINDER;
-		gbc_btnHelp.insets = new Insets(0, 0, 5, 0);
-		gbc_btnHelp.gridx = 0;
-		gbc_btnHelp.gridy = 2;
-		MainMenu.add(btnHelp, gbc_btnHelp);		
 		JButton btnExit = new JButton("Exit");
+		GridBagConstraints gbc_btnExit = new GridBagConstraints();
+		gbc_btnExit.fill = GridBagConstraints.HORIZONTAL;;
+		gbc_btnExit.insets = new Insets(0, 0, 0, 5);
+		gbc_btnExit.gridx = 0;
+		gbc_btnExit.gridy = 4;
+		panel_17.add(btnExit, gbc_btnExit);
 		btnExit.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				System.exit(0);
 			}
 		});
-		GridBagConstraints gbc_btnExit = new GridBagConstraints();
-		gbc_btnExit.anchor = GridBagConstraints.CENTER;
-		gbc_btnExit.gridwidth = GridBagConstraints.REMAINDER;
-		gbc_btnExit.insets = new Insets(0, 0, 0, 0);
-		gbc_btnExit.gridx = 0;
-		gbc_btnExit.gridy = 3;
-		MainMenu.add(btnExit, gbc_btnExit);
 		
 		JPanel CreateDocument_1 = new JPanel();
 		contentPane.add(CreateDocument_1, "CreateDocument_1");
@@ -329,7 +350,6 @@ public class GUI extends JFrame {
 		btnCreate.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				//TODO: create document button
 				try {
 					cw.writeInitialContent(textArea.getText());
 					writeAllContent();
@@ -462,7 +482,6 @@ public class GUI extends JFrame {
 		btnCreate_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				//TODO: create template button
 				String fileSeparator = System.getProperty("file.separator");
 				tw = new TemplateWriter(textField_2.getText() + fileSeparator + StringUtil.replaceExtension(textField_1.getText(), "html"));
 				appendAllTemplateContent();
@@ -526,6 +545,135 @@ public class GUI extends JFrame {
 		gbl_panel_5.rowWeights = new double[]{Double.MIN_VALUE};
 		panel_5.setLayout(gbl_panel_5);
 		CreateTemplate.add(scrollPane_1, BorderLayout.CENTER);
+		
+		JPanel SearchPanel = new JPanel();
+		contentPane.add(SearchPanel, "SearchPanel");
+		SearchPanel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_11 = new JPanel();
+		SearchPanel.add(panel_11, BorderLayout.SOUTH);
+		
+		JButton btnBack_3 = new JButton("Back");
+		btnBack_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				clearSearch();
+				CardLayout cl = (CardLayout)(contentPane.getLayout());
+				cl.show(contentPane, "MainMenu");
+			}
+		});
+		panel_11.add(btnBack_3);
+		
+		JButton btnHelp_4 = new JButton("Help");
+		btnHelp_4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//TODO: help
+				createHelpDialog("Click the Select Directory button to choose the directory where the search will be performed.\n" + 
+						"\n" + 
+						"Enter your query in the Query field.\n" + 
+						"\n" + 
+						"Click Search to perform a search for the given query in the selected directory. Click Back to go back to the previous menu. Click Help to display this text again.");
+			}
+		});
+		panel_11.add(btnHelp_4);
+		
+		JButton btnSearch = new JButton("Search");
+		btnSearch.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if (!(textField_5.equals(""))) {
+					s = new Searcher(textField_5.getText());
+					try {
+						clearSearchResults();
+						ArrayList<String> results = s.search(textField_6.getText());
+						appendSearchResults(panel_16, results);
+					} catch (IOException e) {
+					}
+				} else {
+					createMessageDialog("Please select a directory.");
+				}
+			}
+		});
+		panel_11.add(btnSearch);
+		
+		JPanel panel_12 = new JPanel();
+		SearchPanel.add(panel_12, BorderLayout.NORTH);
+		GridBagLayout gbl_panel_12 = new GridBagLayout();
+		gbl_panel_12.columnWidths = new int[]{212, 212, 0};
+		gbl_panel_12.rowHeights = new int[]{23, 23, 23, 0};
+		gbl_panel_12.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_12.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel_12.setLayout(gbl_panel_12);
+		
+		JButton btnSelectDirectory_2 = new JButton("Select Directory");
+		btnSelectDirectory_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String filePath = createFileChooser(true);
+				textField_5.setText(filePath);
+			}
+		});
+		GridBagConstraints gbc_btnSelectDirectory_2 = new GridBagConstraints();
+		gbc_btnSelectDirectory_2.fill = GridBagConstraints.BOTH;
+		gbc_btnSelectDirectory_2.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSelectDirectory_2.gridx = 0;
+		gbc_btnSelectDirectory_2.gridy = 0;
+		panel_12.add(btnSelectDirectory_2, gbc_btnSelectDirectory_2);
+		
+		textField_5 = new JTextField();
+		textField_5.setEditable(false);
+		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
+		gbc_textField_5.fill = GridBagConstraints.BOTH;
+		gbc_textField_5.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_5.gridx = 1;
+		gbc_textField_5.gridy = 0;
+		panel_12.add(textField_5, gbc_textField_5);
+		textField_5.setColumns(10);
+		
+		JLabel lblQuery = new JLabel("Query");
+		lblQuery.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lblQuery = new GridBagConstraints();
+		gbc_lblQuery.fill = GridBagConstraints.BOTH;
+		gbc_lblQuery.insets = new Insets(0, 0, 5, 5);
+		gbc_lblQuery.gridx = 0;
+		gbc_lblQuery.gridy = 1;
+		panel_12.add(lblQuery, gbc_lblQuery);
+		
+		textField_6 = new JTextField();
+		GridBagConstraints gbc_textField_6 = new GridBagConstraints();
+		gbc_textField_6.fill = GridBagConstraints.BOTH;
+		gbc_textField_6.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_6.gridx = 1;
+		gbc_textField_6.gridy = 1;
+		panel_12.add(textField_6, gbc_textField_6);
+		textField_6.setColumns(10);
+		
+		JPanel panel_13 = new JPanel();
+		SearchPanel.add(panel_13, BorderLayout.CENTER);
+		panel_13.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_14 = new JPanel();
+		panel_13.add(panel_14, BorderLayout.NORTH);
+		
+		JLabel lblResults = new JLabel("Results:");
+		panel_14.add(lblResults);
+		
+		JPanel panel_15 = new JPanel();
+		panel_13.add(panel_15, BorderLayout.CENTER);
+		panel_15.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		panel_15.add(scrollPane_2);
+		
+		panel_16 = new JPanel();
+		scrollPane_2.setViewportView(panel_16);
+		GridBagLayout gbl_panel_16 = new GridBagLayout();
+		gbl_panel_16.columnWidths = new int[]{0};
+		gbl_panel_16.rowHeights = new int[]{0};
+		gbl_panel_16.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_panel_16.rowWeights = new double[]{Double.MIN_VALUE};
+		panel_16.setLayout(gbl_panel_16);
 	}
 	
 	private String createFileChooser(boolean directoriesOnly) {
@@ -640,6 +788,22 @@ public class GUI extends JFrame {
 		}
 	}
 	
+	private void appendSearchResults(JPanel panel, ArrayList<String> results) {
+		int gridy = 0;
+		for (String s : results) {
+			JLabel label = new JLabel(s);
+			GridBagConstraints gbc_label = new GridBagConstraints();
+			gbc_label.insets = new Insets(0, 0, 5, 0);
+			gbc_label.gridx = 0;
+			gbc_label.gridy = gridy;
+			panel.add(label, gbc_label);
+			gridy++;
+			
+			panel.revalidate();
+			validate();
+		}
+	}
+	
 	private void clearCreateDocument() {
 		textField.setText("");
 		textField_4.setText("");
@@ -656,6 +820,19 @@ public class GUI extends JFrame {
 		textField_1.setText("");
 		textField_2.setText("");
 		headingYPos = 0;
+	}
+	
+	private void clearSearch() {
+		textField_5.setText("");
+		textField_6.setText("");
+		clearSearchResults();
+		validate();
+	}
+	
+	private void clearSearchResults() {
+		panel_16.removeAll();
+		panel_16.revalidate();
+		validate();
 	}
 	
 }
